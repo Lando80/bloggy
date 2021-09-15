fdf44d3f7df9ba7b327ed49dabebe8f60872ad40
Lando80 <lando.s@hotmail.com>
2021-09-15 08:48:36 -0400

    chore: setting up mongoose, dovenv, database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 import routes from './routes';
+
+dotenv.config();
 
 const server = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(
+    `mongodb+srv://lando:${process.env.DB_PASSWORD}@cluster0.iwn59.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+
 
 server.use(express.json());
 server.use(routes);
