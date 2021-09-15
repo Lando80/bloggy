3dbcbbf3588bd5e6b397288754c8251533522524
Lando80 <lando.s@hotmail.com>
2021-09-15 11:51:05 -0400

    feat: hashing password befor create user

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import UserController from './controllers/UserController'; 
 
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    response.status(201).json({ menssagem: 'Olá enfermeira!' })
-})
+// routes.get('/', (request, response) => {
+//    response.status(201).json({ menssagem: 'Olá enfermeira!' })
+// })
+
+routes.post('/users', UserController.store)
+
 
 export default routes
 
